0ab0343a8c153137b2a997b0ec1c4a3ddbd286de
JulianFernando <juliancambraia@gmail.com>
2019-08-11 19:46:54 -0300

    Adição do banco de test H2 com criação automática da base de dados.

--- src/main/java/br/com/juliancambraia/cursomc/domain/Categoria.java
+++ src/main/java/br/com/juliancambraia/cursomc/domain/Categoria.java
@@ -1,12 +1,25 @@
 package br.com.juliancambraia.cursomc.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "categoria")
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String nome;
 
     public Categoria() {
