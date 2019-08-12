eb76aeaceeb56dde767a043a6d86a55b55f6f20c
JulianFernando <juliancambraia@gmail.com>
2019-08-11 22:23:04 -0300

    Criando operação de instanciação das categorias

--- src/main/java/br/com/juliancambraia/cursomc/CursomcApplication.java
+++ src/main/java/br/com/juliancambraia/cursomc/CursomcApplication.java
@@ -1,13 +1,39 @@
 package br.com.juliancambraia.cursomc;
 
+import br.com.juliancambraia.cursomc.domain.Categoria;
+import br.com.juliancambraia.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public CursomcApplication(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    /**
+     * Implementação provisória pois estamos usando banco de dados em memória H2 e simulando as inserções
+     * 
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
